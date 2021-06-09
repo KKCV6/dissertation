b3eccae6a5e3f09e154bb186592ea008d9d916da
Holly Mizser-Jones <Holly.Mizser-Jones@arup.com>
2021-06-09 17:12:42 +0100

    commit 2

--- settlement_outlines.R
+++ settlement_outlines.R
@@ -4,11 +4,8 @@ library(tidyverse)
 library(sf)
 library(magrittr)
 library(tmap)
-library(fpc)
 library(dbscan)
-library(sfnetworks)
 library(tidygraph)
-library(igraph)
 library(spNetwork)
 library(stplanr)
 library(tidyverse)
@@ -18,207 +15,237 @@ library(devtools)
 library(foot)
 library(concaveman)
 library(rgeos)
+tmap_mode("view")
 
-#0) Setting Up - JW Ignore my edits up here!
-#footprints
-tnz_bf <- st_read('data/footprints/Tanzania_2019-09-16/Tanzania.geojson')
 
-#boundaries
-tnz_districts <- st_read('data/tanzania_gadm/gadm36_TZA_2.shp')
+#Step 0) Setting Up
+  #footprints
+  tnz_bf <- st_read('data/footprints/Tanzania_2019-09-16/Tanzania.geojson')
 
-#filter
-monduli_district <- filter(tnz_districts, NAME_2 == "Monduli")
-monduli_bf <- st_read('data/footprints/monduli_building_footprints.shp')
+  #boundaries
+  tnz_districts <- st_read('data/boundaries/tanzania_gadm/gadm36_TZA_2.shp')
 
-#centroids
-monduli_bf_centroids <- st_centroid(monduli_bf)
+  #filter
+  monduli_district <- filter(tnz_districts, NAME_2 == "Monduli")
+  monduli_bf <-  tnz_bf[monduli_district,]
 
-#points
-monduli_points <- st_coordinates(monduli_bf_centroids)
 
-#1) Settlement Outlines from Building Footprints
-monduli_dbscan <- dbscan::dbscan(monduli_points, eps = 0.001, minPts = 5)
-plot(monduli_points, col=monduli_dbscan$cluster)
-plot(monduli_district$geometry, add=T)
+#Step 1) Create initial settlement outlines from building footprints
+  
+  #centroids
+  centroids <- st_centroid(monduli_bf)
+  
+  #points
+  points <- st_coordinates(centroids)
+  
+  #DBScan
+  dbscan <- dbscan::dbscan(points, eps = 0.001, minPts = 5)
+  plot(points, col=dbscan$cluster)
+  plot(monduli_district$geometry, add=T)
 
   #assign building footprint to a cluster
-monduli_points_wcluster <- as.data.frame(monduli_points) %>% mutate(dbcluster=monduli_dbscan$cluster)
+  points_wcluster <- as.data.frame(points) %>% mutate(dbcluster=dbscan$cluster)
 
  #Create settlement outlines from Building Footprints and DB-Scan cluster
+  geometry_list <- vector(mode = "list", length = max(points_wcluster$dbcluster))
 
-geometry_list <- vector(mode = "list", length = max(monduli_points_wcluster$dbcluster))
+  counter <-1
 
-counter <-1
-
-for (cluster_index in seq(1, max(monduli_points_wcluster$dbcluster))) {
+  for (cluster_index in seq(1, max(points_wcluster$dbcluster))) {
   
-  monduli_points_wcluster_subset <- filter(monduli_points_wcluster, dbcluster==cluster_index)
+  points_wcluster_subset <- filter(points_wcluster, dbcluster==cluster_index)
   
-  monduli_points_wcluster_subset_coords <- monduli_points_wcluster_subset[c("X", "Y")]
+  points_wcluster_subset_coords <- points_wcluster_subset[c("X", "Y")]
   
-  concave_outline <- as.matrix(monduli_points_wcluster_subset_coords)  %>% concaveman()
+  concave_outline <- as.matrix(points_wcluster_subset_coords)  %>% concaveman()
   
   coords <- as.data.frame(concave_outline)
   
-  settlement_polygon <- coords %>% st_as_sf(coords = c("V1", "V2"), crs = 4326) %>% summarise(geometry = st_combine(geometry)) %>% st_cast("POLYGON")
+  polygon <- coords %>% st_as_sf(coords = c("V1", "V2"), crs = 4326) %>% summarise(geometry = st_combine(geometry)) %>% st_cast("POLYGON")
   
-  geometry_list[counter] <- (settlement_polygon$geometry)
+  geometry_list[counter] <- (polygon$geometry)
   
   counter <- counter + 1
 }
 
-settlement_hulls <- st_sfc(geometry_list, crs = 4326)
+  hulls <- st_sfc(geometry_list, crs = 4326)
 
-st_write(settlement_hulls, "data/settlement_outlines/monduli_settlements_v1.shp", delete_layer = TRUE)
+  st_write(hulls, "data/settlement_outlines/bf_outlines.shp", delete_layer = TRUE)
 
-monduli_settlements_v1 <- st_read('data/settlement_outlines/monduli_settlements_v1.shp')
+  bf_outlines <- st_read('data/settlement_outlines/bf_outlines.shp')
 
-monduli_settlements_v1 <- st_transform(monduli_settlements_v1, crs = 4326)
+  bf_outlines <- st_transform(bf_outlines, crs = 4326)
 
-tmap_mode("view")
-
-tm_shape(monduli_settlements_v1) + tm_borders()
-
-  #buffer
-# I would definitely look into adding a buffer
-
-settlement_buffer <- st_transform(monduli_settlements_v1, crs = 32737)
-
-settlement_buffer<- st_buffer(settlement_buffer, 100,  endCapStyle = "FLAT", joinStyle = "MITRE") %>% st_union()
-
-settlement_buffer <- st_cast(settlement_buffer, "POLYGON")
-
-settlement_buffer<-  settlement_buffer %>%
- st_sf %>%
-st_cast()
-
-tmap_mode("view")
-
-tm_shape(settlement_buffer)+tm_borders()
-
-#2) Create catchment areas from the building outline footprints
+  tm_shape(bf_outlines) + tm_borders()
+  
+#Step 2) Create catchment areas from the building outline footprints
 
   #centroids
-sett_centroids <- st_point_on_surface(monduli_settlements_v1)
-tm_shape(monduli_settlements_v1) + tm_borders() + tm_shape(sett_centroids)+tm_dots()
+  centroids <- st_point_on_surface(bf_outlines)
+  tm_shape(bf_outlines) + tm_borders() + tm_shape(centroids)+tm_dots()
 
   #points
-sett_points <- st_coordinates(sett_centroids)
+  points <- st_coordinates(centroids)
 
-plot(sett_points)
+  plot(points)
 
   #dbscan
-sett_dbscan <- dbscan::dbscan(sett_points, eps = 0.011, minPts = 3) 
+  dbscan <- dbscan::dbscan(points, eps = 0.0119, minPts = 3) 
+  plot(points, col=dbscan$cluster)
+  plot(monduli_district$geometry, add=T)
 
-plot(sett_points, col=sett_dbscan$cluster)
-plot(monduli_district$geometry, add=T)
+  #assign building footprint to a cluster
+  points_wcluster <- as.data.frame(points) %>% mutate(dbcluster=dbscan$cluster)
 
-#assign building footprint to a cluster
-# Keep as sett_points (do not use centroids version as we need the X and Y column)
-sett_points_wcluster <- as.data.frame(sett_points) %>% mutate(dbcluster=sett_dbscan$cluster)
+  geometry_list <- vector(mode = "list", length = max(points_wcluster$dbcluster))
 
-geometry_list <- vector(mode = "list", length = max(sett_points_wcluster$dbcluster))
+  counter <-1
 
-counter <-1
-
-for (cluster_index in seq(1, max(sett_points_wcluster$dbcluster))) {
+  for (cluster_index in seq(1, max(points_wcluster$dbcluster))) {
   
-  sett_points_wcluster_subset <- filter(sett_points_wcluster, dbcluster==cluster_index)
+    points_wcluster_subset <- filter(points_wcluster, dbcluster==cluster_index)
   
-  sett_points_wcluster_subset_coords <- sett_points_wcluster_subset[c("X", "Y")]
+    points_wcluster_subset_coords <- points_wcluster_subset[c("X", "Y")]
   
-  concave_outline <- as.matrix(sett_points_wcluster_subset_coords)  %>% concaveman()
+    concave_outline <- as.matrix(points_wcluster_subset_coords)  %>% concaveman()
   
-  coords <- as.data.frame(concave_outline)
+    coords <- as.data.frame(concave_outline)
   
-  settlement_polygon <- coords %>% st_as_sf(coords = c("V1", "V2"), crs = 4326) %>% summarise(geometry = st_combine(geometry)) %>% st_cast("POLYGON")
+    polygon <- coords %>% st_as_sf(coords = c("V1", "V2"), crs = 4326) %>% summarise(geometry = st_combine(geometry)) %>% st_cast("POLYGON")
 
-  geometry_list[counter] <- (settlement_polygon$geometry)
+    geometry_list[counter] <- (polygon$geometry)
   
   counter <- counter + 1
 }
 
-settlement_hulls <- st_sfc(geometry_list, crs = 4326)
+  hulls <- st_sfc(geometry_list, crs = 4326)
 
-st_write(settlement_hulls, "data/settlement_outlines/settlement_outlines_v2.shp", delete_layer = TRUE)
+  st_write(hulls, "data/settlement_outlines/catch_outlines.shp", delete_layer = TRUE)
 
-sett_outlines <- st_read('data/settlement_outlines/settlement_outlines_v2.shp')
+  catch_outlines <- st_read('data/settlement_outlines/catch_outlines.shp')
 
-tm_shape(sett_outlines)+tm_borders()
+tm_shape(catch_outlines) + tm_borders()+
+  tm_shape(bf_outlines)+tm_polygons(col = "purple")
 
-# Once plotted, I think there is obvious work in improving the settlement outline - I'd look into buffers as above and intersections to join outlines together.
+#Step 3) Asign the building footprint settlement outlines a catchment ID. Note: we will deal with NA values later
 
-tmap_mode("view")
+  mon_sp <- as(bf_outlines, 'Spatial')
 
-tm_shape(sett_outlines) + tm_borders() +
-  tm_shape(monduli_settlements_v1) + tm_borders()
+  sett_sp <- as(catch_outlines, 'Spatial')
 
-#aggregate
+  over_ID <- over(mon_sp, sett_sp, fn=NULL)
 
-mon_sp <- as(monduli_settlements_v1, 'Spatial')
+  bf_outlines$over_id <- over_ID$FID
 
-sett_sp <- as(sett_outlines, 'Spatial')
+  tm_shape(bf_outlines)+
+    tm_polygons(col = "over_id")+
+  tm_shape(catch_outlines)+
+    tm_borders()
 
-over_ID <- over(mon_sp, sett_sp, fn=NULL)
+#Step 4) Create the final settlement outlines, grouping smaller polygons (step 1) by their catchment (step 2)
 
-monduli_settlements_v1$over_id <- over_ID$FID
+  catchments <- bf_outlines %>% group_by(over_id) %>% st_cast("POINT")  
 
-tm_shape(monduli_settlements_v1)+tm_polygons(col = "over_id")+
-  tm_shape(sett_outlines)+tm_borders()
+  catchments <- catchments %>% drop_na(over_id) %>% st_as_sf() #drop NA points as these are standalone settlements
 
-mon_sp <- as(monduli_settlements_v1, 'Spatial')
+  tm_shape(catchments)+
+    tm_dots()
 
-zones.IDs <- as.numeric(monduli_settlements_v1[["over_id"]])
+  points <- st_coordinates(catchments)
 
-points <-  monduli_settlements_v1 %>%  group_by(over_id) %>% st_cast("MULTIPOINT")
+  plot(points)
 
-tm_shape(points)+tm_dots()
+  #catchment clusters
+  plot(points, col=catchments$over_id)
+  plot(monduli_district$geometry, add=T)
 
-poly_points <- st_coordinates(points)
+  #assign to a cluster
+  points_wcluster <- as.data.frame(points) %>% mutate(cluster=catchments$over_id)
 
-#
+  geometry_list <- vector(mode = "list", length = max(points_wcluster$cluster))
 
-polygons_sett <- map(unique(points$over_id),
-                   ~ concaveman(points[points$over_id %in% .,], concavity = 3)
-) %>%
-  map2(unique(points$over_id), ~ mutate(.x, k = .y)) %>% reduce(rbind)
+  counter <-1
 
-polygons_sett <- st_zm(polygons_sett, drop=T, what='ZM')
-
-class(polygons_sett)
-
-tmap_mode("view")
-
-tm_shape(polygons_sett)+tm_fill("over_id",colorNA = NULL)+
-  tm_shape(monduli_settlements_v1)+tm_borders()
-
-polygons_sett <- polygons_sett %>% drop_na(over_id) %>% st_as_sf()
-
-# Difference
+  for (cluster_index in seq(1, max(points_wcluster$cluster))) {
   
-mon_na <- monduli_settlements_v1[is.na(monduli_settlements_v1$over_id),] %>% st_
+    points_wcluster_subset <- filter(points_wcluster, cluster==cluster_index)
+  
+    points_wcluster_subset_coords <- points_wcluster_subset[c("X", "Y")]
+  
+    concave_outline <- as.matrix(points_wcluster_subset_coords)  %>% concaveman()
+  
+    coords <- as.data.frame(concave_outline)
+  
+    polygon <- coords %>% st_as_sf(coords = c("V1", "V2"), crs = 4326) %>% summarise(geometry = st_combine(geometry)) %>% st_cast("POLYGON")
+  
+    geometry_list[counter] <- (polygon$geometry)
+  
+    counter <- counter + 1
+}
 
-mon_na <- st_transform(mon_na, crs = 4326)
+  hulls <- st_sfc(geometry_list, crs = 4326)
 
-polygons_sett <- st_transform(polygons_sett, crs = 4326)
+  st_write(hulls, "data/settlement_outlines/settlement_outlines.shp", delete_layer = TRUE)
 
-#merge
+  settlement_outlines <- st_read('data/settlement_outlines/settlement_outlines.shp')
 
-st_write(polygons_sett, "data/settlement_outlines/polygons_sett.shp", delete_layer = TRUE)
+  tm_shape(settlement_outlines)+
+    tm_polygons(col = "purple", alpha = 0.5)+
+  tm_shape(bf_outlines)+
+    tm_borders()
 
-polygons_sett <- st_read('data/settlement_outlines/polygons_sett.shp')
+  #Difference analysis to extract standalone settlements
+  
+  catch_na <- bf_outlines[is.na(bf_outlines$over_id),] %>% st_as_sf()
 
-st_write(mon_na, "data/settlement_outlines/mon_na.shp", delete_layer = TRUE)
+  catch_na <- st_transform(catch_na, crs = 4326)
 
-mon_na <- st_read('data/settlement_outlines/mon_na.shp')
+  #merge the NA outlines with those within catchments to build a complete dataset
 
-polygons_sett$k=NULL
-mon_na$FID=NULL
+  settlement_outlines <- st_transform(settlement_outlines, crs = 4326)
+  
+  st_write(catch_na, "data/settlement_outlines/catch_na.shp", delete_layer = TRUE)
 
-outlines <- rbind(polygons_sett, mon_na)
+  catch_na <- st_read('data/settlement_outlines/catch_na.shp')
 
-tm_shape(outlines)+tm_borders()
+    #check for matching columns, we will create new settlement IDs
+  catch_na$over_id=NULL 
 
-outlines$ID <- 1:nrow(outlines)
+  outlines <- rbind(settlement_outlines, catch_na)
 
+  tm_shape(outlines)+tm_borders()
 
+  outlines$ID <- 1:nrow(outlines)
+
+  #buffer to create your final outlines in line with good practice
+
+  outline_buffer <- st_transform(outlines, crs = 32737)
+  
+  outline_buffer<- st_buffer(outline_buffer, 150,  endCapStyle = "FLAT", joinStyle = "MITRE") %>% st_union()
+
+  outline_buffer <- st_cast(outline_buffer, "POLYGON")
+
+  final_outlines<-  outline_buffer %>% st_sf %>% st_cast()
+  
+  final_outlines$ID <- 1:nrow(final_outlines)
+
+    #have a look at our final outlines
+  
+   tm_shape(final_outlines)+
+    tm_polygons(col = "purple", alpha = 0.5)
+   
+   #export
+   st_write(final_outlines, "data/settlement_outlines/final_outlines.shp", delete_layer = TRUE)
+   
+   final_outlines <- st_read('data/settlement_outlines/final_outlines.shp')
+
+#Step 5) Calculate areas
+   
+   final_outlines$shape_area <- area(final_outlines) #not yet working
+
+#Step 6) Ground truth against GRID 3 data
+   
+   # List feature classes
+   rgdal::ogrListLayers('data/ground-truth/GRID/GRID3_TZA_SettlementExtents_V01Alpha/GRID3_TZA_settlement_extents_20200425.gdb')
+   
+   ssa_extents <- rgdal::readOGR(dsn = "data/ground-truth/GRID/GRID3_TZA_SettlementExtents_V01Alpha/GRID3_TZA_settlement_extents_20200425.gdb", layer = "ssa_extents")
